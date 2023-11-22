9aaf75aee7a4dcf158e5a531f6e1b0f51593f07d
flavian-anselmo <anselmoflavian4@gmail.com>
2023-11-22 11:35:59 +0300

    update: ssh

--- ssh.py
+++ ssh.py
@@ -1,37 +1,24 @@
-from crontab import CronTab
 import paramiko
-import logging
 
 def ssh_connect(hostname, username, password, port):
     try:
         client = paramiko.SSHClient()
         client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
         client.connect(hostname, port=port, username=username, password=password)
-        logging.info(f'Info: client {username} connected succesfully')
+        print('Successfull ssh')
         return client
     except Exception as error:
-        logging.error(f'Error: {error}')
+        print(f'{error}')
 
 
-
-def configure_cronjob(cron_expression, command, remote_username):
-    try:
-        cron = CronTab(user=remote_username)  
-        job = cron.new(command=command)
-        job.setall(cron_expression)
-        cron.write()
-        logging.info(f'Info: cronjob configured successfully..')
-
-    except Exception as error:
-        logging.error(f'Error: {error}')
 
 
 def disconnect(ssh_client):
     try:
         ssh_client.close()
-        logging.info('SSH session closed...')
+        print('ssh session closed')
     except Exception as error:
-        logging.error(f'Error closing SSH session: {error}')
+        print(f'{error}')
 
 
 
@@ -44,9 +31,6 @@ if __name__ == "__main__":
     #  ssh connection 
     ssh_client = ssh_connect(remote_host, remote_username, remote_password, remote_port)
 
-    cron_expression = '*/1 * * * *'
-    command = '/path/to/your/test.py' 
+    if ssh_client:
+        disconnect(ssh_client)
 
-    # configure a cronjob 
-    configure_cronjob(cron_expression, command,remote_username)
-    # disconnect(ssh_client.close())
